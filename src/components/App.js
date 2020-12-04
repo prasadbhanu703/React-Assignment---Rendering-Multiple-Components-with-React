a155aaceaf9f0c3c0ae3586e889880224852ebe0
prasadbhanu703 <prasadbhanu703@gmail.com>
2020-12-04 18:32:00 +0530

    chnaged 2

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+        <h3 data-ns-test="project-name"> New Project </h3>
+        <p data-ns-test="project-description"> Description of projects belongs here.......!!! </p>
+    </div>
   )
 }
 
